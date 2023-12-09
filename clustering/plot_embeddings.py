b3d5be7b27efa1e62faab84c5e5e1fab3b071d06
Will Liang <willjhliang@gmail.com>
2023-12-09 00:02:12 -0500

    plot embeddings

--- clustering/plot_embeddings.py
+++ clustering/plot_embeddings.py
@@ -17,13 +17,21 @@ def plot_embeddings(embeddings, labels):
         label_colors = np.argmax(labels, axis=1)
     else:
         label_colors = labels
-    plt.scatter(output[:, 0], output[:, 1], c=label_colors)
+    
+    for i in range(np.max(label_colors) + 1):
+        print(f"Size of class {i}: {np.sum(label_colors == i)}")
+    
+    plt.rcParams["image.cmap"] = "Spectral"
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    scatter = ax.scatter(output[:, 0], output[:, 1], c=label_colors)
+    legend1 = ax.legend(*scatter.legend_elements(num=None), loc="upper right", title="Classes")
+    ax.add_artist(legend1)
     plt.show()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--embedding_type", type=str)
+    parser.add_argument("-e", "--embedding_type", type=str, required=True, help="embedding type, one of the names in embeddings/")
     args = parser.parse_args()
 
-    embeddings, labels = load_embeddings(args.embedding_type)
+    embeddings, labels, _ = load_embeddings(args.embedding_type)
     plot_embeddings(embeddings, labels)
